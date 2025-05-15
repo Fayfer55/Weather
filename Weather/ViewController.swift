317affa5ff186b4722890375eaed32356c673f14
Kirill Faifer <sayfer555@gmail.com>
2025-05-16 00:44:14 +0600

    add CLLocation authorization request with check

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -6,13 +6,43 @@
 //
 
 import UIKit
+import CoreLocation
+import OSLog
 
 final class ViewController: UIViewController {
+    
+    private let logger = Logger(subsystem: Bundle.main.bundleIdentifier ?? "Weather.app", category: "ViewController")
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
+        
+        Task {
+            await checkLocationAuthorization()
+        }
+    }
+    
+    // MARK: - Private Helpers
+    
+    private func checkLocationAuthorization() async {
+        let session = CLServiceSession(authorization: .whenInUse)
+        
+        do {
+            for try await diagnostic in session.diagnostics {
+                guard !diagnostic.authorizationRequestInProgress else { continue }
+                
+                if diagnostic.authorizationDenied {
+                    logger.debug("denied -> use moscow location")
+                } else {
+                    logger.debug("allowed -> request location position")
+                }
+            }
+        } catch {
+            logger.error("\(error)")
+        }
     }
 
 }
