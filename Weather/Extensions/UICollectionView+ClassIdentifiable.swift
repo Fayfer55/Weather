bfeb56cef1254474c8feb8ce019f26c5b5268e45
Kirill Faifer <sayfer555@gmail.com>
2025-05-20 19:22:19 +0600

    add UICollectionView+ClassIdentifiable extension

---
+++ Weather/Extensions/UICollectionView+ClassIdentifiable.swift
@@ -0,0 +1,39 @@
+//
+//  UICollectionView+ClassIdentifiable.swift
+//  Weather
+//
+//  Created by Kirill Faifer on 20.05.2025.
+//
+
+import UIKit.UICollectionView
+
+extension UICollectionView {
+
+    func register<T: UICollectionViewCell>(cellType: T.Type) where T: ClassIdentifiable {
+        register(cellType.self, forCellWithReuseIdentifier: cellType.reuseId)
+    }
+    
+    func dequeueReusableCell<T: UICollectionViewCell>(withCellType type: T.Type = T.self, for indexPath: IndexPath) -> T where T: ClassIdentifiable {
+        guard let cell = dequeueReusableCell(withReuseIdentifier: type.reuseId, for: indexPath) as? T
+        else { fatalError(dequeueError(reuseId: type.reuseId)) }
+
+        return cell
+    }
+    
+    func cellForItem<T: UICollectionViewCell>(withCellType type: T.Type = T.self, at indexPath: IndexPath) -> T where T: ClassIdentifiable {
+        guard let cell = cellForItem(at: indexPath) as? T
+        else { fatalError(dequeueError(reuseId: type.reuseId)) }
+        return cell
+    }
+
+}
+
+// MARK: - Dequeue Error Method
+
+extension UICollectionView {
+
+    private func dequeueError(reuseId: String) -> String {
+        return "Couldn't dequeue cell with identifier \(reuseId)"
+    }
+
+}
