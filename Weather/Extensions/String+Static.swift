85c036499c407765de29e9b9bcb0bba165a177d0
Kirill Faifer <sayfer555@gmail.com>
2025-05-20 19:57:39 +0600

    add String + Static extension

---
+++ Weather/Extensions/String+Static.swift
@@ -0,0 +1,14 @@
+//
+//  String+Static.swift
+//  Weather
+//
+//  Created by Kirill Faifer on 20.05.2025.
+//
+
+import Foundation
+
+extension String {
+    
+    static let degree = "\u{00B0}"
+    
+}
